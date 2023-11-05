a3dbcf4b52a93c2765b4dcd01e7ee7e9d55fbcc4
Gaston <gastonallerdev@gmail.com>
2023-11-04 21:14:30 -0300

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'One page gy',
+  title: 'One page gym',
   description: 'One page gym',
 }
 
