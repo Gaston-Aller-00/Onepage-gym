c701f60cd681a3ef914bf2b0da4886c26ef3036d
Gaston <gastonallerdev@gmail.com>
2023-11-22 12:45:41 -0300

    menu

--- components/shared/main-menu.tsx
+++ components/shared/main-menu.tsx
@@ -1,40 +1,37 @@
 import Link from "next/link";
-import ButtonPrimary from "./ui/button-primary";
+import ButtonPrimary from "../ui/button-primary";
 
 const MainMenu = () => {
   return (
-    <ul className="flex items-center gap-4 ">
+    <ul className="flex flex-col lg:flex-row items-center gap-4 ">
       <li>
         <Link
-          href="#home"
-          className="py-2 px-4 border-b-2  border-primary transition-colors duration-300 text-white "
+          href='#home'
+          className='py-2 px-4 border-b-2 border-primary transition-colors duration-300 text-white'
         >
           Home
         </Link>
       </li>
       <li>
         <Link
-          href="#services"
-          className="py-2 px-4 border-b-2 border-transparent hover:border-primary transition-colors 
-          duration-300 text-white"
+          href='#services'
+          className='py-2 px-4 border-b-2 border-transparent hover:border-primary transition-colors duration-300 text-white'
         >
           Services
         </Link>
       </li>
       <li>
         <Link
-          href="#about"
-          className="py-2 px-4 border-b-2 border-transparent hover:border-primary transition-colors 
-          duration-300 text-white"
+          href='#about'
+          className='py-2 px-4 border-b-2 border-transparent hover:border-primary transition-colors duration-300 text-white'
         >
           About
         </Link>
       </li>
       <li>
         <Link
-          href="#contact"
-          className="py-2 px-4 border-b-2 border-transparent hover:border-primary transition-colors 
-          duration-300 text-white"
+          href='#contact'
+          className='py-2 px-4 border-b-2 border-transparent hover:border-primary transition-colors duration-300 text-white'
         >
           Contact
         </Link>
