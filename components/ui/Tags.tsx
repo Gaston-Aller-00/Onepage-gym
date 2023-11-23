af0375284a50c10f9968da81139a49eae11ad8c7
Gaston <gastonallerdev@gmail.com>
2023-11-23 15:31:41 -0300

    Tags component with conditional

---
+++ components/ui/Tags.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/libs/utils";
+import { FC } from "react";
+
+//tags components con el condicional para la que lleve el active se ponga con el color primary 
+interface TagsProps {
+  text: string;
+  active?: boolean;
+}
+
+const Tags: FC<TagsProps> = ({ text, active }) => {
+  return (
+    <span
+    className={cn(
+      "py-1 px-2 rounded bg-gray-light",
+      active && "bg-primary text-white"
+    )}
+  >
+    {text}
+  </span>
+  );
+};
+
+export default Tags;
