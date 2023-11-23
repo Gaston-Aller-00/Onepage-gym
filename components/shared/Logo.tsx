0937c9ba62fdec846eeef2b87c4a1bd993f204e6
Gaston <gastonallerdev@gmail.com>
2023-11-23 15:36:00 -0300

    logo

--- components/shared/Logo.tsx
+++ components/shared/Logo.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link'
-
+// logo enviar al hero
 const Logo = () => {
     return ( <Link href="/" type="button" className='text-white'>
         GYM
     </Link> );
 }
  
+//todo poner logo lindo
 export default Logo;
