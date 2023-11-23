3b0af5314c20fbe6920b88d46953efc09318d999
Gaston <gastonallerdev@gmail.com>
2023-11-23 15:31:53 -0300

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+import About from '@/components/about'
 import Hero from '@/components/Hero'
+import Plans from '@/components/Plans'
 import Services from '@/components/Services'
+import Trainers from '@/components/Trainers'
 import React from 'react'
 
 type Props = {}
@@ -8,6 +11,9 @@ const Page = (props: Props) => {
   return (
     <div><Hero/> 
     <Services/>
+    <Plans/>
+    <Trainers/>
+    <About/>
     </div>
   )
 }
