8197b12b0743250493509ce886c23800ef5857c6
Gaston <gastonallerdev@gmail.com>
2023-11-23 15:32:35 -0300

    section footer component

---
+++ components/footer/components/section.tsx
@@ -0,0 +1,27 @@
+
+//footer section indivual componentizada
+//title y array de strings
+
+import {FC} from 'react'
+interface SectionProps {
+    title:string;
+    list:string[];
+
+}
+
+
+
+const Section:FC<SectionProps>  = ({title,list}) => {
+    return ( <section>
+        <h5 className="text-2xl font-semibold text-white mb-5"> {title}</h5>
+        <ul>
+         {list.map((link,index) => (
+
+          <li key={index} className="mb-2">{link}</li>
+         ))}
+          
+        </ul>
+      </section> );
+}
+ 
+export default Section;
