cc7fd752a6de94fe6a6c9ebd538403522924b0d6
Gaston <gastonallerdev@gmail.com>
2023-11-23 15:35:01 -0300

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,28 @@
-import Header from '@/components/shared/Header'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
+import Header from "@/components/shared/Header";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Footer from "@/components/footer";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'One page gym',
-  description: 'One page gym',
-}
+  title: "One page gym",
+  description: "One page gym",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={`${inter.className} bg-black text-white`}>
-      <Header/>
-        {children}</body>
+        <Header />
+
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
