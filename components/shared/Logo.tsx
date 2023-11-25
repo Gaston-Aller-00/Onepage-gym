4b0d90cdc6d765543cf64426641faf58693d3e57
Gaston <gastonallerdev@gmail.com>
2023-11-24 21:23:18 -0300

    fix

--- components/shared/Logo.tsx
+++ components/shared/Logo.tsx
@@ -2,8 +2,9 @@ import Link from 'next/link'
 import Image from 'next/image';
 // logo enviar al hero
 const Logo = () => {
-    return ( <Link href="/" type="button" className='text-white'>
-        <Image
+    return ( <Link href="/" type="button" className='text-white '>
+       <Image
+        className='hover:boxShadow'
         src="/images/logo.png" 
         alt="logo"
         width={80} 
